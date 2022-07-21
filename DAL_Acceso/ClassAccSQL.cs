aa72f4695b4fb7f6263f08ab363c30d09fd92a7b
Sarah Aguilar <utp0138594@alumno.utpuebla.edu.mx>
2022-07-21 14:58:44 -0500

    Se agrego classe ClassAccSQL y se realizo metodo AbrirConexion()

---
+++ DAL_Acceso/ClassAccSQL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Acceso
+{
+    public class ClassAccSQL
+    {
+
+        private string conexion;
+
+        public ClassAccSQL(string cadcon)
+        {
+            conexion = cadcon;
+        }
+        public SqlConnection AbrirConexion(ref string mensaje) 
+        {
+            SqlConnection conAbierta = new SqlConnection(); //Conexion a la base de datos Covid 
+            conAbierta.ConnectionString = conexion;
+
+            try
+            {
+                conAbierta.Open(); //Abre BD
+                mensaje = "Conexión abierta CORRECTAMENTE";
+            }
+            catch (Exception r)
+            {
+                conAbierta = null; //Devuelve una conexion nula
+                mensaje = "Error: " + r.Message;
+            }
+            return conAbierta;
+        }
+    }
+}
